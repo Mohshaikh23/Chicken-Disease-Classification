e51535b3ad8490a94bf34e29652f846eea9a3def
Mohsin Shaikh <mohsin.shaikh324@gmail.com>
2023-06-05 00:22:19 -0700

    Requiremnets.txt added

--- template.py
+++ template.py
@@ -22,7 +22,8 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "main.py"
+    "main.py",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
