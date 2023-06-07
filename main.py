35a63301b5e2f9b83e4c2b850506a624a168aa4e
Mohsin Shaikh <mohsin.shaikh324@gmail.com>
2023-06-07 00:18:02 -0700

    Training Module is Ready

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from CNN_Classifier import logger
 from CNN_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNN_Classifier.pipeline.stage_02_prepare_base_model import PreparebaseModeltrainingPipeline
-
+from CNN_Classifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -22,3 +22,14 @@ try:
 except Exception as e:
       logger.exception(e)
       raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
